a3b62769c4417d27af0d53d5f0347066f34b19f5
Karl Isenberg <karl@mesosphere.io>
2015-06-20 13:55:53 -0700

    Abstract run_in_docker function for mesos-docker cluster

---
+++ cluster/mesos/docker/deploy-addons.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Copyright 2015 The Kubernetes Authors All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# deploy the add-on services after the cluster is available
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+KUBE_ROOT=$(cd "$(dirname "${BASH_SOURCE}")/../../.." && pwd)
+source "${KUBE_ROOT}/cluster/${KUBERNETES_PROVIDER}/${KUBE_CONFIG_FILE-"config-default.sh"}"
+
+if [ "${ENABLE_CLUSTER_DNS}" == true ]; then
+  echo "Deploying DNS on kubernetes" 1>&2
+  sed -e "s/{{ pillar\['dns_replicas'\] }}/${DNS_REPLICAS}/g;s/{{ pillar\['dns_domain'\] }}/${DNS_DOMAIN}/g" $KUBE_ROOT/cluster/addons/dns/skydns-rc.yaml.in > dns/skydns-rc.yaml
+  sed -e "s/{{ pillar\['dns_server'\] }}/${DNS_SERVER_IP}/g" $KUBE_ROOT/cluster/addons/dns/skydns-svc.yaml.in > dns/skydns-svc.yaml
+
+  KUBECONFIG=$("${KUBE_ROOT}/cluster/kubectl.sh" config view | base64)
+  sed -e "s,KUBECONFIG,$KUBECONFIG," dns/skydns-secret.yaml.in > dns/skydns-secret.yaml
+
+  # use kubectl to create skydns rc and service
+  "${KUBE_ROOT}/cluster/kubectl.sh" create -f dns/skydns-secret.yaml
+  "${KUBE_ROOT}/cluster/kubectl.sh" create -f dns/skydns-rc.yaml
+  "${KUBE_ROOT}/cluster/kubectl.sh" create -f dns/skydns-svc.yaml
+fi

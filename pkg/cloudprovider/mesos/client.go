d6e200871e67e9e8b255cb48a533f311d6b0da19
k8s-merge-robot <k8s.production.user@gmail.com>
2015-10-17 02:20:18 -0700

    Merge pull request #15735 from mesosphere/jdef-fix-tasks-and-profiling
    Auto commit by PR queue bot
    Conflicts:
    	cluster/mesos/docker/docker-compose.yml

--- pkg/cloudprovider/mesos/client.go
+++ pkg/cloudprovider/mesos/client.go
@@ -102,7 +102,14 @@ func newMesosClient(
 	md detector.Master,
 	mesosHttpClientTimeout, stateCacheTTL time.Duration) (*mesosClient, error) {
 
-	tr := &http.Transport{}
+	tr := &http.Transport{
+		Proxy: http.ProxyFromEnvironment,
+		Dial: (&net.Dialer{
+			Timeout:   30 * time.Second,
+			KeepAlive: 30 * time.Second,
+		}).Dial,
+		TLSHandshakeTimeout: 10 * time.Second,
+	}
 	httpClient := &http.Client{
 		Transport: tr,
 		Timeout:   mesosHttpClientTimeout,
